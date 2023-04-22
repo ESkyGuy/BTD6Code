2e216de6b7f53707eafee98136e5e4d3b2d0a82f
ESkyGuy <131508872+ESkyGuy@users.noreply.github.com>
2023-04-22 00:42:40 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,81 @@
+import time
+
+import pyautogui
+
+while True:
+    # Loading the Game
+
+    time.sleep(5)
+
+    pyautogui.leftClick(1100, 1200, duration=0.5)
+
+    pyautogui.leftClick(1800, 1200, duration=2)
+
+    pyautogui.leftClick(1800, 1200, duration=0.3)
+
+    pyautogui.leftClick(1850, 300, duration=0.5)
+
+    pyautogui.leftClick(800, 500, duration=0.5)
+
+    pyautogui.leftClick(1700, 600, duration=0.5)
+
+    time.sleep(5)
+
+    pyautogui.leftClick(1250, 1000, duration=1)
+
+    time.sleep(1)
+
+    # Churchill Spot
+
+    pyautogui.leftClick(2280, 282, duration=0)
+
+    pyautogui.leftClick(776, 822, duration=0)
+
+    # Super Monkey
+
+    pyautogui.moveTo(2250, 1200, duration=0)
+
+    pyautogui.dragTo(2280, 10, duration=0.3)
+
+    time.sleep(0.5)
+
+    pyautogui.leftClick(2450, 440, duration=0)
+
+    pyautogui.moveTo(741, 635, duration=0)
+
+    pyautogui.leftClick(741, 635, duration=0)
+
+    pyautogui.leftClick(741, 633, duration=0)
+
+    pyautogui.leftClick(2000, 1000, duration=0)
+
+    pyautogui.leftClick(2000, 1000, duration=0)
+
+    pyautogui.leftClick(2000, 1000, duration=0)
+
+    pyautogui.leftClick(2000, 650, duration=0)
+
+    pyautogui.leftClick(2000, 650, duration=0)
+
+    # Timer Start
+
+    pyautogui.leftClick(2500, 1300, duration=0)
+
+    pyautogui.leftClick(2500, 1300, duration=0)
+
+    # Extra
+
+    pyautogui.leftClick(2280, 1250)
+    pyautogui.leftClick(980, 550)
+    pyautogui.leftClick(980, 550)
+    pyautogui.leftClick(2069, 840)
+    pyautogui.leftClick(2069, 840)
+    pyautogui.leftClick(2069, 840)
+
+    # Resetting
+
+    time.sleep(368)
+
+    pyautogui.leftClick(1300, 1200, duration=1)
+
+    pyautogui.leftClick(900, 1150, duration=1)
